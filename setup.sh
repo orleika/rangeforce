0c299497616349e8f95ffbad1b0869dc379514d9
orleika <orleika.net@gmail.com>
2016-10-29 13:45:25 +0900

    Tweak

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install latest version node.js, if not found node command
-type node > /dev/null 2>&1 || curl -L https://git.io/n-install | bash
+type node > /dev/null 2>&1 || curl -L https://git.io/n-install | bash -s -- -y
 export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
 
 # install bower, which is frontend package manager
